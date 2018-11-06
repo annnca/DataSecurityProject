35c8c433ae6f320ad38d0689e0f94e2c9973d2c1
Anca Stanasel <stanasel.anca@yahoo.com>
2018-11-06 23:38:12 +0200

    Update vigenereLib.h

--- vigenereLib.h
+++ vigenereLib.h
@@ -1,8 +1,17 @@
 #ifndef VIGENERELIB_H_INCLUDED
 #define VIGENERELIB_H_INCLUDED
 
+/*The multiplied key*/
+extern char key[50];
+/*The text after encryption was performed*/
+extern char enc[50];
+/*The text after decryption was performed*/
+extern char dec[50];
+/*Function to perform encryption*/
 char* vigenereEncrypt(char*  text, char*  key);
-char*  vigenereDecrypt(char*  text, char*  key);
-char*  vigenereKey(char*  text, char*  oneWordKey);
+/*Function to perform decryption*/
+char* vigenereDecrypt(char*  text, char*  key);
+/*This function multiplies the key in order to have the same length as the message*/
+char* vigenereKey(char*  text, char*  oneWordKey);
 
 #endif // VIGENERELIB_H_INCLUDED
